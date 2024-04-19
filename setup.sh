5e6fed041d03b0feb31a8808eddc127f119f158a
Jungk0 <106706369+Jungk0@users.noreply.github.com>
2024-04-19 14:40:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,18 @@
 sudo dnf -y upgrade
 sudo dnf reinstall linux-firmware
 sudo dnf group install "Hardware Support"
+sudo dnf install kitty hyprland-devel cmake cpio meson hyprpm udiskie wofi swaybg sddm firefox waybar dunst hypridle wlogout
+
+# hycov
+# git clone https://github.com/DreamMaoMao/hycov.git /tmp/hycov
+# cd /tmp/hycov || exit
+# bash install.sh
+# cd "$LOCAL_PATH"
+
+# swaylock
+sudo dnf copr enable eddsalkield/swaylock-effects -y
+sudo dnf install -y swaylock-effects
+
+# swaync
+sudo dnf copr enable eddsalkield/SwayNotificationCenter -y
+sudo dnf install -y SwayNotificationCenter
